57c4140a62baddae09c4775cd5da54f8d9ec371f
Gustavo <gustavo3.silva@usp.br>
2015-11-18 11:57:23 -0200

    Ajustes no spinner

--- app/src/main/java/br/com/kronos/kronos/HistoryActivity.java
+++ app/src/main/java/br/com/kronos/kronos/HistoryActivity.java
@@ -2,21 +2,25 @@ package br.com.kronos.kronos;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.github.mikephil.charting.charts.PieChart;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import br.com.kronos.database.KronosDatabase;
 import br.com.kronos.kronos.adapters.ListAtividadesAdapter;
+import br.com.kronos.listener.HistorySpinnerPeriodListener;
 
 public class HistoryActivity extends Activity {
 
-    private KronosDatabase kronosDatabase;
+    public KronosDatabase kronosDatabase;
     private Spinner period;
     private Spinner format;
+    public List<Atividade> activityList;
 
     //private ListHistoryActivitiesAdapter listAtividadesAdapter;
     private PieChart pieChart;
@@ -28,8 +32,13 @@ public class HistoryActivity extends Activity {
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
         kronosDatabase = new KronosDatabase(this);
+        activityList = new LinkedList<>();
 
-
+        period = (Spinner) findViewById(R.id.spinner_selectPeriod);
+        String[] period_options = getResources().getStringArray(R.array.spinner_period_options);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, period_options);
+        period.setOnItemSelectedListener(new HistorySpinnerPeriodListener(activityList, kronosDatabase));
+        dataAdapter.setDropDownViewResource(R.layout.spinner_item);
     }
 
 }
