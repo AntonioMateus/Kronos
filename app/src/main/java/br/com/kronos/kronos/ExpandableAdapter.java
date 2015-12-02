9abe768baf71ce5dca34301f6e35230e71df9fff
antonio <antoniocms1995@hotmail.com>
2015-12-02 11:21:14 -0200

    método de cálculo do progresso adicionado à classe KronosDatabase

--- app/src/main/java/br/com/kronos/kronos/ExpandableAdapter.java
+++ app/src/main/java/br/com/kronos/kronos/ExpandableAdapter.java
@@ -1,7 +1,105 @@
 package br.com.kronos.kronos;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+import br.com.kronos.database.KronosDatabase;
+
 /**
  * Created by antonio on 01/12/15.
  */
-public class ExpandableAdapter {
+public class ExpandableAdapter extends BaseExpandableListAdapter {
+    private List<String> listGroup;
+    private HashMap<String, List<Meta>> listData;
+    private LayoutInflater inflater;
+    private Context context;
+
+    public ExpandableAdapter(Context context, HashMap<String, List<Meta>> listData, List<String> listGroup) {
+        this.context = context;
+        this.listGroup = listGroup;
+        this.listData = listData;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public int getGroupCount() {
+        return listGroup.size();
+    }
+
+    public int getChildrenCount(int groupPosition) {
+        return listData.get(listGroup.get(groupPosition)).size();
+    }
+
+    public Object getGroup(int groupPosition) {
+        return listGroup.get(groupPosition);
+    }
+
+    public Object getChild(int groupPosition, int childPosition) {
+        return listData.get(listGroup.get(groupPosition)).get(childPosition);
+    }
+
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        ViewHolderGroup holder;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.header_expandable_list_view,null);
+            holder = new ViewHolderGroup();
+            convertView.setTag(holder);
+
+            holder.tvGroup = (TextView) convertView.findViewById(R.id.tvGroup);
+        }
+        else {
+            holder = (ViewHolderGroup) convertView.getTag();
+        }
+        holder.tvGroup.setText(listGroup.get(groupPosition));
+        return convertView;
+    }
+
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        ViewHolderItem holder;
+        Meta meta = (Meta) getChild(groupPosition,childPosition);
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item_expandable_list_view,null);
+            holder = new ViewHolderItem();
+            convertView.setTag(holder);
+            holder.progressBar_goal = (ProgressBar) convertView.findViewById(R.id.progressBar_goal);
+        }
+        else {
+            holder = (ViewHolderItem) convertView.getTag();
+        }
+        KronosDatabase database = new KronosDatabase(this.context);
+        holder.progressBar_goal.setIndeterminate(false);
+        holder.progressBar_goal.setProgress(database.devolveProgressoMeta(meta.getDescricao()));
+        return convertView;
+    }
+
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    class ViewHolderGroup {
+        TextView tvGroup;
+    }
+
+    class ViewHolderItem {
+        ProgressBar progressBar_goal;
+    }
 }
